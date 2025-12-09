388d97dc8965a076539455e8c32ad3bc0050e10b
P. Thịnh <pthinh.tsr@proton.me>
2025-12-09 17:48:22 +0700

    src: config.py: Use Ollama's configuration instead
    The model is more stable/accurate with this:
    - Adding num_predictm num_ctx cause strange accuracy decreases
    - White repeat_last_n, repeat_penalty causes infinite looping (ironic)

--- src/config.py
+++ src/config.py
@@ -11,13 +11,9 @@ OLLAMA_MODEL = "deepseek-ocr:3b"
 # DeepSeek-OCR expects 1024x1024 inputs
 TARGET_IMAGE_SIZE = (1024, 1024)
 
-# Configuration from vLLM-Inference example
+# Configuration from Ollama Modelfile
 INFERENCE_PARAMS = {
     "temperature": 0,
-    "num_predict": 8192,
-    "num_ctx": 8192,
-    "repeat_last_n": -1, # This is *inf of what VLLM uses (window_size=90).
-    "repeat_penalty": 1.2, # Strongly discourage model infinite looping (only work sometimes).
 }
 
 PROMPTS = {
